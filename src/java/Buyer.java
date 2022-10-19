8168863f3be5e4585640cab8f31324a771f748df
BhargavRamThatikonda <bthatiko@asu.edu>
2022-10-18 23:42:30 -0700

    Given astah files

---
+++ src/java/Buyer.java
@@ -0,0 +1,18 @@
+package java;
+
+public class Buyer extends Person {
+
+	private Person person;
+
+	public void showMenu() {
+
+	}
+
+	/**
+	 *  
+	 */
+	public ProductMenu CreateProductMenu() {
+		return null;
+	}
+
+}
